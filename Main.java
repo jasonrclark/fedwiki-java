18202e304fc5d05a0733a05bac17943f79e5e48e
Jason R. Clark <jason@jasonrclark.net>
2023-04-18 10:14:35 -0700

    Basic read and follow link loop

--- Main.java
+++ Main.java
@@ -5,39 +5,65 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
   public static void main(String... args) throws URISyntaxException, IOException, InterruptedException {
-    String url = "http://found.ward.bay.wiki.org/json-schema.json";
+//    String url = "http://found.ward.bay.wiki.org/json-schema.json";
+    String url = "http://ward.dojo.fed.wiki/dojo-practice-yearbooks.json";
     if (args.length > 0) {
       url = args[1];
     }
 
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(new URI(url))
-            .header("User-Agent", "fedwiki-java")
-            .version(HttpClient.Version.HTTP_1_1)
-            .GET()
-            .build();
+    while (true) {
+      HttpRequest request = HttpRequest.newBuilder()
+              .uri(new URI(url))
+              .header("User-Agent", "fedwiki-java")
+              .version(HttpClient.Version.HTTP_1_1)
+              .GET()
+              .build();
 
-    HttpResponse<String> response = HttpClient
-            .newBuilder()
-            .build()
-            .send(request, HttpResponse.BodyHandlers.ofString());
+      HttpResponse<String> response = HttpClient
+              .newBuilder()
+              .build()
+              .send(request, HttpResponse.BodyHandlers.ofString());
 
-    var mapper = new ObjectMapper();
+      var mapper = new ObjectMapper();
 
-    var result = mapper.readValue(response.body(), Page.class);
+      var result = mapper.readValue(response.body(), Page.class);
 
-    System.out.println("# title");
-    System.out.println(result.title);
-    for (Item item: result.story) {
+      var scanner = new Scanner(System.in);
+
+      System.out.println("# title");
+      System.out.println(result.title);
       System.out.println("");
-      System.out.println("# "+item.type);
-      System.out.println(item.text);
+
+      for (Item item : result.story) {
+        System.out.println("# " + item.type);
+        System.out.println(item.text);
+
+        var cmd = scanner.nextLine();
+        if (cmd.equals("exit")) {
+          System.exit(0);
+        } else if (cmd.equals("l")) {
+          if (item.links().size() > 0) {
+            var title = item.links().get(0);
+            var slug = title.replaceAll("\\s", "-")
+                    .replaceAll("[^A-Za-z0-9-]", "")
+                    .toLowerCase(Locale.getDefault());
+
+            url = String.format("http://ward.dojo.fed.wiki/%s.json", slug);
+            break;
+          } else {
+            System.out.println("No link in this item");
+          }
+        }
+      }
     }
   }
 
@@ -54,6 +80,17 @@ public class Main {
     public String id;
     public String title;
     public String text;
+
+    private static final Pattern linkPattern = Pattern.compile(".*\\[\\[(.*)]].*");
+
+    public List<String> links() {
+      var matcher = linkPattern.matcher(text);
+      if (matcher.find()) {
+        return List.of(matcher.group(1));
+      }
+
+      return List.of();
+    }
   }
 
   @JsonIgnoreProperties(ignoreUnknown = true)

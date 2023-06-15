154bb13200d82ed6c9dfa07b84b2e96950ab42ce
Jeffrey Miller <jeffrey.miller@gmail.com>
2023-06-14 21:31:42 -0700

    add site arg to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # run with standard input as commands
-# usage: sh run.sh
+# usage: sh run.sh [site]
 
 java \
   -cp jackson-core-2.14.2.jar:jackson-annotations-2.14.2.jar:jackson-databind-2.14.2.jar \
-  Main.java
+  Main.java $1
